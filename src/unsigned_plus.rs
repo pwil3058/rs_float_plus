028b6440f65b2b331f6888bae84c972e8c92565d
Peter Williams <pwil3058@gmail.com>
2020-02-11 15:17:04 +1000

    unsigned_plus: handle u16, u32 and u64
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/unsigned_plus.rs
+++ src/unsigned_plus.rs
@@ -35,3 +35,39 @@ impl crate::NumberConstants for u16 {
     const TWO: Self = 2;
     const THREE: Self = 3;
 }
+
+/// Constants that are applicable to numbers in general
+impl crate::NumberConstants for u32 {
+    /// How many bytes does this type occupy
+    const BYTES: usize = 4;
+    /// Approximate number of significant digits in base 10.
+    const DIGITS: u32 = 10;
+    /// Largest finite value for this type.
+    const MAX: Self = std::u32::MAX;
+    /// Smallest finite value for this type.
+    const MIN: Self = std::u32::MIN;
+
+    /// Other values useful in generic code
+    const ZERO: Self = 0;
+    const ONE: Self = 1;
+    const TWO: Self = 2;
+    const THREE: Self = 3;
+}
+
+/// Constants that are applicable to numbers in general
+impl crate::NumberConstants for u64 {
+    /// How many bytes does this type occupy
+    const BYTES: usize = 8;
+    /// Approximate number of significant digits in base 10.
+    const DIGITS: u32 = 20;
+    /// Largest finite value for this type.
+    const MAX: Self = std::u64::MAX;
+    /// Smallest finite value for this type.
+    const MIN: Self = std::u64::MIN;
+
+    /// Other values useful in generic code
+    const ZERO: Self = 0;
+    const ONE: Self = 1;
+    const TWO: Self = 2;
+    const THREE: Self = 3;
+}
